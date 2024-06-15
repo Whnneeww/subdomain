80e385ce3d463e98aaca2f7b07d6c4a402f764c9
name-y <Whnneeww@outlook.jp>
2024-06-15 13:15:57 +0900

    Delete create_subdomain directory

--- create_subdomain/index.py
+++
@@ -1,6 +0,0 @@
-input_string = input("Enter a string: ")  # ユーザーから文字列を入力 
- 
-# 文字列を小文字に変換し、スペースをハイフンに置換してサブドメインを作成 
-subdomain = input_string.lower().replace(" ", "-") 
- 
-print("Subdomain created: " + subdomain) 
